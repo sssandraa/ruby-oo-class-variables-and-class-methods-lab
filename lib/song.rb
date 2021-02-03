f6c24ecb8de5cd3f943e85ea090b5d0d4778cd75
Sandra Wazaz <sandrawazaz@gmail.com>
2021-02-03 10:15:24 -0500

    commit tree c608f8966a5f74846caa769b2e2ceea53892e347

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song 
+attr_accessor :name, :artist, :genre 
+@@count = 0
+
+def initialize (name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1
+end 
+def self.count 
+    @@count 
+end 
+
+end 
